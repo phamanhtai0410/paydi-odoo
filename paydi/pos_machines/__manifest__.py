c5f350999bfc09d1f5a39afbca2c0a2847f6da2d
khoabd <khoabd98@gmail.com>
2021-11-26 00:34:11 +0700

    init app: pos machines and master data

---
+++ paydi/pos_machines/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Pos Machines',
+    'version': '0.1',
+    'summary': 'Summery',
+    'description': 'Description',
+    'category': 'Sales',
+    'depends': ['mail'],
+    'data': [
+        'views/pos_machines_view.xml',
+        'security/ir.model.access.csv'
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True
+}
