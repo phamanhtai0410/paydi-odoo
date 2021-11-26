af11b0f3fa546f13699585f91602fb94a3da1dd0
PAT <phamanhtai0410@gmail.com>
2021-11-26 16:42:26 +0700

    init report transactions

---
+++ paydi/report_transactions/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Transactions Report',
+    'category': 'Productivity',
+    'sequence': -100,
+    'description': """Transactions Report: Listing""",
+    'summary': 'Transactions Report: Listing',
+    'depends': ['base'],
+    'license': 'LGPL-3',
+    'data': [
+        ''
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True
+}
+
---
+++ paydi/report_transactions/models/transactions.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import models, fields
+
+
+class Transactions(models.Model):
+    _name = 'transactions'
+    _description = 'Detail of Transactions'
+
+    company_vn_name = fields.Char(string='Tên đăng ký(VN)')
+    company_en_name = fields.Char(string='Tên đăng ký(EN)')
+
+    obj_type = fields.Selection([
+        ('merchant', 'Đối tác máy Pos'),
+    ], string="Loại liên hệ")
+
+    business_type = fields.Selection([
+        ('C_TNHH', 'Công ty TNHH'),
+        ('C_CP', 'Công ty Cổ phần'),
+    ], string='Loại hình công ty')
+
+    registration_number = fields.Char(string="Số đăng ký kinh doanh")
+
+    service_ids = fields.One2many('m.service', 'partner_id', string="Dịch vụ khác")
+
