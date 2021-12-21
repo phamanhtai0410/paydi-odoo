f22705930973b51910d489202dd15029bab06f1b
khoabd <khoabd98@gmail.com>
2021-12-21 14:19:25 +0700

    update app: merchant contact

--- paydi/merchant_contacts/__manifest__.py
+++ paydi/merchant_contacts/__manifest__.py
@@ -6,7 +6,7 @@
     'name': 'Quản lý merchant',
     'category': 'Sales',
     'summary': '',
-    'depends': ['base', 'mail', 'account'],
+    'depends': ['base', 'mail', 'account', 'vn.location'],
     'data': [
         'views/res_partner_views.xml',
         'views/merchant_service.xml',
