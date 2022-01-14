dd2dd554148d9d69b3a5b6bc302d6bbc41931cb2
PAT <phamanhtai0410@gmail.com>
2022-01-14 12:01:52 +0700

    _develop/report_13_ Add company_code AUTO SET when create a company

--- paydi/crm_mgmt/models/res_company.py
+++ paydi/crm_mgmt/models/res_company.py
@@ -8,3 +8,12 @@ class ResCompany(models.Model):
     crm_url = fields.Char(string='URL của CRM')
     username = fields.Char(string='Username')
     password = fields.Char(string='Password')
+    company_code = fields.Char(string='Company Code', readonly=True, compute="_get_company_code")
+    
+    @api.depends('name')
+    def _get_company_code(self):
+        for each in self:
+            each.company_code  = each.name.lower()
+        
+    
+    
