6b678d030c18169c3d6ea2652e6359d075412c6b
khanhnv07 <khanhnv100398@gmail.com>
2022-04-07 14:20:59 +0700

    _3_develop/suppoer_mms_v1_'modify template and btn scrap'

---
+++ paydi/hide_btn_scrap/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'hide btn scrap',
+    'category': 'Sales',
+    'summary': '',
+    'depends': ['base','crm'],
+    'data': [
+        'views/hide_btn_scrap.xml'
+    ],
+    'application': True
+}
