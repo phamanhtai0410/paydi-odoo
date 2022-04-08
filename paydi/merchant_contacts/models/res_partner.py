7d991ed9804c64eb16748d186c9635d955cc68f1
khanhnv07 <khanhnv100398@gmail.com>
2022-04-08 17:01:53 +0700

    _6_develop/support_mms_v1_'modify sale and purchase'

--- paydi/merchant_contacts/models/res_partner.py
+++ paydi/merchant_contacts/models/res_partner.py
@@ -70,11 +70,11 @@ class ResPartner(models.Model):
                                               related='supporter_id.department_id')
 
     support_employee_team = fields.Many2one('crm.team',
-                                                string="Đội ngủ chăm sóc")
+                                                string="Đội ngũ chăm sóc")
     
     supporter_employee_id = fields.Many2one('res.users',
-                                              string="Nhân viên chăm sóc khách hàng",
-                                              related='support_employee_team.user_id'
+                                              string="Nhân viên chăm sóc",
+                                              domain="[('sale_team_id', '=', support_employee_team)]"
                                               )
     
     profile_status = fields.Selection([ ('1', 'Đang đàm phán'),('2', 'Đã ký hợp đồng'),('3','Đã cấp máy')],'Trạng thái hồ sơ', default='1')
