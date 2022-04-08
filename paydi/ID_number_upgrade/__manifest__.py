e3cdcd3531a452dd631976e326ad7a9913f59984
tinanhtom <tinanhtom02@gmail.com>
2022-04-08 15:13:52 +0700

    _5_develop/suppoer_mms_v1_'upgrade ID Number module '

--- paydi/ID_number_upgrade/__manifest__.py
+++ paydi/ID_number_upgrade/__manifest__.py
@@ -1,15 +1,10 @@
+
 {
-    'name': 'Partner Identification Numbers Upgrade ',
-    'version': '0.1',
-    'summary': 'Summery',
-    'description': 'Description',
-    'category': "Customer Relationship Management",
-    'depends': ['contacts'],
-    'data': [
-        'views/res_partner_id_number_view.xml',
-    ],
-    'demo': [],
-    'installable': True,
-    'auto_install': False,
-    'application': True
+    "name": "Partner Identification Numbers Upgrade",
+    "category": "Customer Relationship Management",
+    "version": "14.0.1.0.1",
+    "license": "AGPL-3",
+    "depends": ["partner_identification"],
+    "data": ["views/res_partner_id_number_view.xml"],
+    "development_status": "Production/Stable",
 }
--- paydi/ID_number_upgrade/models/res_partner_id_number.py
+++ paydi/ID_number_upgrade/models/res_partner_id_number.py
@@ -1,18 +1,18 @@
 
-from odoo import api, fields, models
 
+from odoo import _, api, models , fields
+from odoo.exceptions import ValidationError
 
 class ResPartnerIdNumber(models.Model):
-    _name = "res.partner.id_number"
     _inherit = "res.partner.id_number"
-
+    _description = "Partner ID Number"
+    _order = "name"
+    
     position = fields.Char(
         string="Chức vụ",
     )
     Representative_information = fields.Char(
         string="Thông tin người đại diện",
-        help="The place where the ID has been issued. For example the country "
-        "for passports and visa",
     )
     contract_payment_date = fields.Date(
         string="Ngày trả hợp đồng cho đơn vị",
