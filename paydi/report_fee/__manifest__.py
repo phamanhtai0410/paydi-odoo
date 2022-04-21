96720913b264e45b29c625848bfba47e0d41f859
khanhnv07 <khanhnv100398@gmail.com>
2022-04-21 10:28:34 +0700

    _46_develop/support_mms_v1_'add module export excel'

---
+++ paydi/report_fee/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'report fee mechant',
+    'category': 'Sales',
+    'summary': '',
+    'depends': ['base', 'mail', 'account', 'vn_administrative', 'merchant_contacts'],
+    'data': [
+        'views/report_fee.xml',
+    ],
+    'application': True
+}
---
+++ paydi/report_fee/models/report_fee_model.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+import json
+import time
+import requests
+import webbrowser
+from odoo import api, models, modules, fields, _
+from odoo.http import request
+
+class ResPartner(models.Model):
+    _name = 'res.partner'
+    _inherit = 'res.partner'
+    
+    def export_action(self):
+        id = self.ids
+        records = self.env["res.partner.fee"].search_read([("partner_id","=",id[0])])
+        if len(records) < 1:
+            fees = []
+            return
+        else :
+            fees = []
+            for record in records:
+                record_obj = {
+                'odoo_contact_id' : record.get('partner_id')[0],
+                'merchant_name':record.get('partner_id')[1],
+                'card_type' : record.get('card_type'),
+                'bank': record.get('bank')[1],
+                'fee': record.get('fee'),
+                'from_date': time.mktime(record.get('from_date').timetuple()),
+                'to_date': time.mktime(record.get('to_date').timetuple())
+                }
+                fees.append(record_obj)
+            
+        fee_obj = {
+            "fees": fees
+        }
+        
+        result = requests.post("https://paydi-staging.rinznetwork.com/v1/data-odoo/transactions_statistic/report_transactions", json=fee_obj);
+        path = result.json().get('data').get('path')
+        webbrowser.open_new_tab(path)
+        return True
