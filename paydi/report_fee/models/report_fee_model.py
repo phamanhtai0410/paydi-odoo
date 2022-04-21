6b8881c63f978d0b7f04160891700d93dab71f01
khanhnv07 <khanhnv100398@gmail.com>
2022-04-21 15:51:52 +0700

    _48_develop/support_mms_v1_'modify api url staging'

--- paydi/report_fee/models/report_fee_model.py
+++ paydi/report_fee/models/report_fee_model.py
@@ -34,7 +34,7 @@ class ResPartner(models.Model):
             "fees": fees
         }
         
-        result = requests.post("http://localhost:5000/v1/data-odoo/transactions_statistic/report_transactions", json=fee_obj);
+        result = requests.post("https://paydi-staging.rinznetwork.com/v1/data-odoo/transactions_statistic/report_transactions", json=fee_obj);
         path = result.json().get('data').get('path')
         webbrowser.open_new_tab(path)
         return True
