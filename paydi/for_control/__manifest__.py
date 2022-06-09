3340db3e8a2f987bb6e7e081d94da2ac9458d154
khanhnguyen1003 <50676880+khanhnguyen1003@users.noreply.github.com>
2022-06-09 10:44:38 +0700

    _65_develop/support_mms_v1_'add module for control'

---
+++ paydi/for_control/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Đối Soát',
+    'category': 'Sales',
+    'description': 'Description',
+    'summary': '',
+    'depends': ['base', 'mail', 'account', 'vn_administrative', 'merchant_contacts'],
+    'data': [
+        'views/for_control_view.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+  
+}
---
+++ paydi/for_control/models/for_control.py
@@ -0,0 +1,9 @@
+from odoo import fields, models, api
+
+
+class ForControl(models.Model):
+    _name = 'for.control'
+    _description = 'Description'
+    # _rec_name = 'name'
+    
+
