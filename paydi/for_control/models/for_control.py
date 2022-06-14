aef9be96832026f90182421fb077af5a284c5978
khanhnguyen1003 <50676880+khanhnguyen1003@users.noreply.github.com>
2022-06-14 10:20:13 +0700

    _69_develop/support_mms_v1_'modify fileds in cross_checking'

--- paydi/for_control/models/for_control.py
+++ paydi/for_control/models/for_control.py
@@ -1,7 +1,10 @@
 from odoo import fields, models, api
+import base64
+from io import StringIO
+from xlrd import open_workbook
+# import xlrd
 
-
-class ForControl(models.Model):
+class ForControl(models.TransientModel):
     _name = 'for.control'
     _description = 'Description'
     _rec_name = 'name'
@@ -11,10 +14,14 @@ class ForControl(models.Model):
     value = fields.Char(string="Giá trị")
     name = fields.Char(string="Hiển thị")
 
-    field_name = fields.Binary(string='Upload File')
+    # import_data_form_file = fields.Binary(string='Upload File')
     bank = fields.Selection([ ('tpbank', 'TP Bank'),('vietcombank', 'Vietcombank'),('BIDV','Đã cấp máy')],'Ngân hàng', default='tpbank')
     spending_account = fields.Selection([ ('123456789', '123456789'),('132654987', '132654987')],'Tài khoản chi', default='123456789')
     method = fields.Selection([ ('noi dia', 'Nội địa'),('quoc te', 'Quốc tế')],'Phương thức thanh toán', default='noi dia')
+
+    # file_name = fields.Binary(string='Upload File')
+    file_data = fields.Binary('File')
+    file_name = fields.Char('File Name')
 
     # def _get_for_control_ids(self):
     #     print('accccc')
@@ -28,9 +35,44 @@ class ForControl(models.Model):
     # transaction_ids = fields.One2many('for.control')
 
     # def action_from_view(self, cr, uid, ids, context=None):
+    # http://www.numberspeaks.com/2018/05/11/import-xlsx-file-odoo-11-sales-orders/
+
+    @api.onchange('file_data')
+    def _onchange_shape(self):
+        print("-------------------seft------------------", self.file_name)
+        print("-------------------input file------------------", self.file_data)
+        # wb = open_workbook(file_contents = base64.decodestring(self.file_name))
+        # sheet = wb.sheets()[0]
+        # print('sheettttttttt =================', sheet)
+        #Decode data
+        if self.file_name:
+            data = base64.b64decode(self.file_data)
+            #Save file
+            with open('/tmp/' + self.file_name, 'wb') as file:
+                file.write(data)
+            xl_workbook = open_workbook(file.name)
+            sheet_names = xl_workbook.sheet_names()
+            xl_sheet = xl_workbook.sheet_by_name(sheet_names[0])
+            num_cols = xl_sheet.ncols
+            print('----------------------num_cold---------------------', num_cols)
+
+            headers = []
+            for col_idx in range(0, num_cols):
+                cell_obj = xl_sheet.cell(0, col_idx)
+                headers.append(cell_obj.value)
+
+            import_data = []
+            for row_idx in range(1, xl_sheet.nrows):    # Iterate through rows
+                row_dict = {}
+                for col_idx in range(0, num_cols):  # Iterate through columns
+                    cell_obj = xl_sheet.cell(row_idx, col_idx)  # Get cell object by row, col
+                    row_dict[headers[col_idx]] = cell_obj.value
+                import_data.append(row_dict)
+            print('-----------------------import_data ----------------', import_data)
+
     def action_from_view(self):
         view_id = self.env.ref('for_control.cross_checking_confirm_view').id
-        print('-----------------------------------------------------ids view-----------------------------', self)
+        print('-----------------------------ids view-----------------------------', self)
         print('view_id', view_id)
         return {
             'type': 'ir.actions.act_window',
