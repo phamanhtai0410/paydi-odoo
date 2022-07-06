27dede283dd14699647bfc8d6dc80dbe66cb88e1
tinanhtom <tinanhtom02@gmail.com>
2022-07-06 17:52:54 +0700

    _87_develop/support_mms_v1_'add stock_move_line_update module'

---
+++ paydi/stock_move_line_update/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'stock move line',
+    'version': '0.1',
+    'summary': 'Summery',
+    'description': 'Description',
+    'category': 'Sales',
+    'depends': ['stock'],
+    'data': [
+        'views/stock_move_line.xml'
+
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True
+}
