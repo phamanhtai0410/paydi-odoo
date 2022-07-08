61627664b49cc600bfa9c6c8ff56faa0a4ffee06
tinanhtom <tinanhtom02@gmail.com>
2022-07-08 10:13:55 +0700

    Revert "Revert "_94_develop/support_mms_v1_'add module export pdf'""
    This reverts commit e2ae3d62b306007710f56f5fc5db7a26849bcd94.

---
+++ paydi/export_pdf/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Export PDF',
+    'category': 'Sales',
+    'description': 'Description',''
+    'summary': '',
+    'depends': ['base', 'merchant_contacts'],
+    'data': [
+        'views/export_pdf_view.xml'
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True
+}
